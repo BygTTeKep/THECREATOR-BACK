8e79b164a11655054f41ab1015e86138d5975fbb
AlexDev <aytuganovv2@gmail.com>
2026-05-30 21:58:35 +0700

    fix/feat: пофиксил отображение картинок и вариантов продуктов для продуктов/добавил oversize

--- src/modules/products/domain/enums/productSize.enum.ts
+++ src/modules/products/domain/enums/productSize.enum.ts
@@ -5,4 +5,15 @@ export enum ProductSizeEnum {
   L = 'L',
   XL = 'XL',
   XXL = 'XXL',
+  OVERSIZE = 'OS',
 }
+
+export const ProductSizeDescriptionEnum = {
+  [ProductSizeEnum.XS]: 'XS',
+  [ProductSizeEnum.S]: 'S',
+  [ProductSizeEnum.M]: 'M',
+  [ProductSizeEnum.L]: 'L',
+  [ProductSizeEnum.XL]: 'XL',
+  [ProductSizeEnum.XXL]: 'XXL',
+  [ProductSizeEnum.OVERSIZE]: 'Oversize',
+};
--- src/modules/products/infrastructure/mappers/getProduct.mapper.ts
+++ src/modules/products/infrastructure/mappers/getProduct.mapper.ts
@@ -3,6 +3,7 @@ import { GetProductOrmDto } from '../dtos/getProductOrm.dto';
 import { GetProductFilesOrmDto } from '../dtos/getProductFilesOrm.dto';
 import { GetProductResponseDto } from '../../presentation/dtos/getProductResponse.dto';
 import { GetProductVariantsOrmDto } from '../dtos/getProductVariantsOrm.dto';
+import { ProductSizeDescriptionEnum } from '../../domain/enums/productSize.enum';
 
 @Injectable()
 export class GetProductMapper {
@@ -11,6 +12,22 @@ export class GetProductMapper {
     productFiles: GetProductFilesOrmDto[],
     productVariants: GetProductVariantsOrmDto[],
   ): GetProductResponseDto {
+    const productFilesDto = productFiles
+      .filter((file) => file.product_id === product.id)
+      .map((file) => ({
+        id: file.id,
+        file_url: file.file_url,
+      }));
+    const productVariantsDto = productVariants
+      .filter((variant) => variant.product_id === product.id)
+      .map((variant) => ({
+        id: variant.id,
+        size: ProductSizeDescriptionEnum[variant.size],
+        sku: variant.sku,
+        price: variant.price,
+        stock: variant.stock,
+      }));
+    console.log(productFilesDto);
     return {
       id: product.id,
       drop_id: product.drop_id,
@@ -18,17 +35,8 @@ export class GetProductMapper {
       base_cost: product.base_cost,
       metadata: product.metadata,
       created_at: product.created_at,
-      files: productFiles.map((file) => ({
-        id: file.id,
-        file_url: file.file_url,
-      })),
-      variants: productVariants.map((variant) => ({
-        id: variant.id,
-        size: variant.size,
-        sku: variant.sku,
-        price: variant.price,
-        stock: variant.stock,
-      })),
+      files: productFilesDto,
+      variants: productVariantsDto,
     };
   }
 }
